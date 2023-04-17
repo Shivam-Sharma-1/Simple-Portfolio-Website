62db5d107b73ea3e808f2a4385111c08b8adf3d0
Shivam77607 <shivamsharma77607@gmail.com>
2023-04-17 16:51:41 +0530

    feat: add about me section

---
+++ src/components/TimeLine/TimeLine.js
@@ -0,0 +1,14 @@
+import { Section, SectionText, SectionTitle } from "@/themes/GlobalComponents"
+
+function TimeLine() {
+    return (
+        <Section id='about'>
+            <SectionTitle>About Me</SectionTitle>
+            <SectionText>
+                I love developing websites and problem solving. I am a quick learner and love to learn new concepts inside and outside of my domain. I am currently tech team member in GDSC MVJCE and pursuing B.E degree(2021-2025) in CSE in MVJ College Of Engineering, Bengaluru, India. I am knowledgeable in basic data structures and algorithms and in frontend web development
+            </SectionText>
+        </Section>
+    )
+}
+
+export default TimeLine
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import BackgroundAnimation from "@/components/BackgroundAnimation/BackgroundAnim
 import Hero from "@/components/Hero/Hero";
 import Projects from "@/components/Projects/Projects";
 import Technologies from "@/components/Technologies/Technologies";
+import TimeLine from "@/components/TimeLine/TimeLine";
 import Layout from "@/layout/Layout";
 import { Section } from "@/themes/GlobalComponents";
 import Head from "next/head";
@@ -22,6 +23,7 @@ export default function Home() {
             </Section>
             <Projects/>
             <Technologies/>
+            <TimeLine/>
         </Layout>
         </>
     )
