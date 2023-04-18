357e8beb86d9628f41c42a0e140be6f2b1529b9a
Shivam77607 <shivamsharma77607@gmail.com>
2023-04-18 20:04:30 +0530

    feat: add responsiveness to timeline section

--- src/components/TimeLine/TimeLineStyles.js
+++ src/components/TimeLine/TimeLineStyles.js
@@ -17,6 +17,15 @@ export const CarouselContainer = styled.ul`
     &::-webkit-scrollbar {
         display: none;
     }
+
+    @media ${props => props.theme.breakpoints.sm} {
+        overflow-x: scroll;
+        -webkit-overflow-scrolling: touch;
+        scroll-snap-type: x mandatory;
+        touch-action: pan-x;
+        justify-content: initial;
+        margin-bottom: 8px;
+    }
 `
 
 export const CarouselMobileScrollNode = styled.div`
@@ -30,6 +39,25 @@ export const CarouselItem = styled.div`
     background: #0F1624;
     border-radius: 3px;
     max-width: 196px;
+
+    @media ${props => props.theme.breakpoints.md} {
+        max-width: 124px;
+    }
+      
+    @media ${props => props.theme.breakpoints.sm} {
+        margin-left: 32px;
+        min-width: 120px;
+        background: #0E131F;
+        padding: 4px;
+        align-content: start;
+        scroll-snap-align: start;
+        border-radius: 3px;
+        overflow: visible;
+        position: relative;
+        height: fit-content;
+        
+        ${(props) => props.active === props.index ? `opacity: 1` : `opacity: 0.5`}; 
+    }
 `
 
 export const CarouselItemTitle = styled.h4`
@@ -43,12 +71,29 @@ export const CarouselItemTitle = styled.h4`
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
     margin-bottom: 8px;
+
+    @media ${props => props.theme.breakpoints.md} {
+        font-size: 20px;
+        line-height: 28px;
+        margin-bottom: 4px;
+    }
+      
+    @media ${props => props.theme.breakpoints.sm} {
+        font-size: 16px;
+        line-height: 24px;
+    }
 `
 
 export const CarouselItemImg = styled.svg`
     margin-left: 21px;
     -webkit-mask-image: linear-gradient(to right, rgba(0,0,0,1), rgba(0,0,0,0));
     width: 100%;
+
+    @media ${props => props.theme.breakpoints.sm} {
+        -webkit-mask-image: none;
+        margin-left: 16px;
+        overflow: visible;
+    }
 `
 
 export const CarouselItemText = styled.p`
@@ -57,12 +102,30 @@ export const CarouselItemText = styled.p`
     letter-spacing: 0.02em;
     color: rgba(255, 255, 255, 0.75);
     padding-right: 16px;
+
+    @media ${props => props.theme.breakpoints.md} {
+        font-size: 12px;
+        line-height: 18px;
+        padding-right: 32px;
+    }
+
+    @media ${props => props.theme.breakpoints.sm} {
+        font-size: 10px;
+        line-height: 16px;
+        padding-right: 0;
+    }
 `
 
 export const CarouselButtons = styled.div`
     width: 288px;
     display: none;
     visibility: hidden;
+
+    @media ${props => props.theme.breakpoints.sm} {
+        display: flex;
+        visibility: visible;
+        margin-bottom: 48px;
+    }
 `
 
 export const CarouselButton = styled.button`
