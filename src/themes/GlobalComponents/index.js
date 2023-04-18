ae8d99613a23001fb3aa460db6125574fabe8b0e
Shivam77607 <shivamsharma77607@gmail.com>
2023-04-18 20:09:04 +0530

    feat: add responsiveness

--- src/themes/GlobalComponents/index.js
+++ src/themes/GlobalComponents/index.js
@@ -10,9 +10,17 @@ export const Section = styled.section`
     position: relative;
     overflow: hidden;
     grid-template-columns: 1fr 1fr;
-    width: calc(100vw - 32px);
-    flex-direction: column;
-  }
+
+    @media ${(props) => props.theme.breakpoints.md} {
+        padding: 24px 48px 0;
+        flex-direction: column;
+    }
+    
+    @media ${(props) => props.theme.breakpoints.sm} {
+        padding: ${(props) => props.nopadding ? "0" : "16px 16px 0" } ;
+        width: calc(100vw - 32px);
+        flex-direction: column;
+    }
 `
 
 export const SectionTitle = styled.h2`
@@ -26,6 +34,23 @@ export const SectionTitle = styled.h2`
     -webkit-text-fill-color: transparent;
     margin-bottom: 16px;
     padding: ${(props) => props.main ? '58px 0 16px' : '0'};
+
+    @media ${props => props.theme.breakpoints.md}{
+        font-size: ${(props) => props.main ? '56px' : '48px'};
+        line-height: ${(props) => props.main ? '56px' : '48px'};
+        margin-bottom: 12px;
+        padding: ${(props) => props.main ? '40px 0 12px' : '0'};
+    }
+    
+    @media ${props => props.theme.breakpoints.sm}{
+        font-size: 32px;
+        line-height: 40px;
+        font-size: ${(props) => props.main ? '28px' : '32px'};
+        line-height: ${(props) => props.main ? '32px' : '40px'};
+        margin-bottom: 8px;
+        padding: ${(props) => props.main ? '16px 0 8px' : '0'};
+        max-width: 100%;
+    }
 `
 
 export const SectionText = styled.p`
@@ -35,6 +60,19 @@ export const SectionText = styled.p`
     font-weight: 300;
     padding-bottom: 3.6rem;
     color: rgba(255, 255, 255, 0.5);
+
+    @media ${(props) => props.theme.breakpoints.md} {
+        max-width: 670px;
+        font-size: 20px;
+        line-height: 32px;
+        padding-bottom: 24px;
+    }
+    
+    @media ${(props) => props.theme.breakpoints.sm} {
+        font-size: 16px;
+        line-height: 24px;
+        padding-bottom: 16px;
+    }
 `
 
 export const SectionDivider = styled.div`
@@ -46,6 +84,16 @@ export const SectionDivider = styled.div`
         'linear-gradient(270deg, #F46737 0%, #945DD6 100%)' :
         'linear-gradient(270deg, #13ADC7 0%, #945DD6 100%)'};
     margin: ${(props) => props.divider ? "4rem 0" : ""};
+
+    @media ${(props) => props.theme.breakpoints.md} {
+        width: 48px;
+        height: 4px;
+    }
+    
+    @media ${(props) => props.theme.breakpoints.sm} {
+        width: 32px;
+        height: 2px;
+    }
 `
 
 export const SectionSubText = styled.p`
@@ -54,6 +102,17 @@ export const SectionSubText = styled.p`
     font-size: 18px;
     line-height: 32px;
     color: rgba(255, 255, 255, 0.75);
+
+    @media ${(props) => props.theme.breakpoints.md} {
+        max-width: 672px;
+        font-size: 16px;
+        line-height: 25px;
+    }
+    
+    @media ${(props) => props.theme.breakpoints.sm} {
+        font-size: 14px;
+        line-height: 22px;
+    }
 `
 
 export const SecondaryBtn = styled.button`
@@ -86,6 +145,24 @@ export const SecondaryBtn = styled.button`
         border: 1px solid #304169;
         box-shadow: inset 0px 2px 1px rgba(46, 49, 55, 0.15), inset 0px 0px 4px rgba(20, 20, 55, 0.3);
     }
+
+    @media ${(props) => props.theme.breakpoints.md}{
+        margin-top: 24px; 
+        margin-bottom: 64px;
+        padding: 16px 24px;
+        width: fit-content;
+        font-size: 20px;
+        line-height: 20px;
+    }
+    
+    @media ${(props) => props.theme.breakpoints.sm} {
+        margin-top: 16px;
+        margin-bottom: 40px;
+        padding: 8px 16px;
+        width: 100%;
+        font-size: 14px;
+        line-height: 16px;
+    }
 `
 
 export const ButtonBack = styled.div`
@@ -105,6 +182,20 @@ export const ButtonBack = styled.div`
     position: relative;
     overflow: hidden;
     opacity: ${({ disabled }) => disabled ? '.5' : '1'};
+
+    @media ${(props) => props.theme.breakpoints.md} {
+        width: ${({ alt }) => alt ? '150px' : '184px'};
+        height: ${({ alt }) => alt ? '52px' : '48px'};
+        font-size: ${({ alt }) => alt ? '20px' : '16px'};
+        margin-bottom: ${({ alt }) => alt ? '0' : '64px'};
+    }
+    
+    @media ${(props) => props.theme.breakpoints.sm} {
+        width: 100%;
+        height: 32px;
+        font-size: 14px;
+        margin-bottom: ${({ alt }) => alt ? '0' : '32px'};
+    }
 `
 
 export const ButtonFront = styled.button`
@@ -143,6 +234,14 @@ export const ButtonFront = styled.button`
         opacity: 0.5;
         box-shadow: inset 0px 2px 1px rgba(46, 49, 55, 0.15), inset 0px 0px 4px rgba(20, 20, 55, 0.3);
     }
+
+    @media ${(props) => props.theme.breakpoints.md} {
+        font-size: ${({ alt }) => alt ? '20px' : '16px'};
+    }
+    
+    @media ${(props) => props.theme.breakpoints.sm} {
+        font-size: 14px;
+    }
 `
 
 export const LinkContainer = styled.div`
@@ -157,9 +256,26 @@ export const LinkContainer = styled.div`
         transform: scale(1.2);
         cursor: pointer;
     }
+
+    @media ${(props) => props.theme.breakpoints.md} {
+        margin-left: ${({ large }) => large ? '16px' : '8px'};
+    
+    }
+
+    @media ${(props) => props.theme.breakpoints.sm} {
+        margin-left: ${({ large }) => large ? '0' : '8px'};
+    }
 `
 
 export const LinkIconImg = styled.div`
     display: flex;  
     height: ${({ large }) => large ? '32px' : '24px'};
+
+    @media ${(props) => props.theme.breakpoints.md} {
+        height: ${({ nav }) => nav ? '16px' : '24px'};
+    }
+    
+    @media ${(props) => props.theme.breakpoints.sm} {
+        height: ${({ large }) => large ? '32px' : '16px'};
+    }
 `
