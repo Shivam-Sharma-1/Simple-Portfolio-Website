9797ddec9307fc843d8a903e52d3fd1a2b279069
Shivam77607 <shivamsharma77607@gmail.com>
2023-04-18 19:50:54 +0530

    feat: add responsiveness to footer section

--- src/components/Footer/FooterStyles.js
+++ src/components/Footer/FooterStyles.js
@@ -6,6 +6,11 @@ export const FooterWrapper = styled.section`
     padding: 2rem 48px 40px;
     margin: 1rem auto;
     box-sizing: content-box;
+
+    @media ${props => props.theme.breakpoints.sm} {
+        padding: 0 16px 48px;
+        width: calc(100vw - 32px);
+    }
 `
 
 export const LinkItem = styled.a`
@@ -16,10 +21,23 @@ export const LinkItem = styled.a`
 	transition: .3s ease;
 	position: relative;
 	left: 0;
-
 	&:hover {
 		color: #fff;
 		left: 6px;
+	}
+
+    @media ${props => props.theme.breakpoints.md} {
+		font-size: 16px;
+		line-height: 28px;
+		display: flex;
+	}
+
+	@media ${props => props.theme.breakpoints.sm} {
+		font-size: 8px;
+		line-height: 14px;
+		margin-bottom: 8px;
+		display: flex;
+		align-items: center;
 	}
 `
 
@@ -27,6 +45,17 @@ export const SocialIconsContainer = styled.div`
     max-width: 1040px;
     display: flex;
     justify-content: space-between;
+
+    @media ${props => props.theme.breakpoints.md}{
+        display: flex;
+        justify-content: space-between;
+    }
+      
+    @media ${props => props.theme.breakpoints.sm}{
+        display: flex;
+          width: 100%;
+        flex-direction: column;
+    } 
 `
 
 export const CompanyContainer = styled.div`
@@ -34,6 +63,18 @@ export const CompanyContainer = styled.div`
 	align-items:baseline;
 	flex-wrap: wrap;
 	margin-right: auto;
+
+    @media ${props => props.theme.breakpoints.md}{
+		flex-direction: column;
+		align-items: baseline;
+	}
+
+	@media ${props => props.theme.breakpoints.sm}{
+		display: flex;
+		flex-direction: column;
+		margin: 0 0 32px;
+		align-items: center;
+	}
 `
 
 export const Slogan = styled.p`
@@ -43,11 +84,28 @@ export const Slogan = styled.p`
 	font-size: 18px;
 	line-height: 30px;
 	padding: 1rem;
+
+    @media ${props => props.theme.breakpoints.md}{
+		font-size: 16px;
+		line-height: 28px;
+	}
+
+	@media ${props => props.theme.breakpoints.sm}{
+		line-height: 22px;
+		font-size: 14px;
+		min-width: 100px;
+	}
 `
 
 export const SocialContainer = styled.div`
 	display: flex;
     align-items: center;
+
+    @media ${props => props.theme.breakpoints.md}{
+		justify-content: center;
+		padding-right: 16px;
+		flex-wrap: wrap;
+	}
 `
 
 export const LinkList = styled.ul`
@@ -56,6 +114,21 @@ export const LinkList = styled.ul`
 	grid-template-columns: repeat(3, minmax(85px, 220px));
 	gap: 40px;
     padding: 40px 0 28px;
+
+    @media ${props => props.theme.breakpoints.lg} {
+		padding: 32px 0 16px;
+	}
+
+	@media ${props => props.theme.breakpoints.md} {
+		width: 100%;
+		padding: 32px 0 16px;
+		gap: 16px;
+	}
+	@media ${props => props.theme.breakpoints.sm} {
+		width: 100%;
+		padding: 32px 4px 16px;
+		gap: 5px;
+	}
 `
 
 export const LinkColumn = styled.div`
@@ -72,4 +145,10 @@ export const LinkTitle = styled.h4`
 	text-transform: uppercase;
 	color: rgba(255, 255, 255, 0.4);
 	margin-bottom: 16px;
+
+    @media ${props => props.theme.breakpoints.sm} {
+		font-size: 10px;
+		line-height: 12px;
+		margin-bottom: 8px;
+	}
 `
